9939436752aa50e14b187f4dd32bace1a9fc567e
mula829 <105131554+mula829@users.noreply.github.com>
2022-08-27 22:00:07 -0400

    Add files via upload

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,4 +1,38 @@
--- Use Dictinct with Orderby to remove duplicate rows
+-- Deliverable 1 Retiring Titles
+
+SELECT ce.emp_no,
+  ce.first_name,
+  ce.last_name,
+  ti.title,
+  ti.from_date,
+  ti.to_date
+INTO retirement_titles
+FROM employees AS ce
+  INNER JOIN titles AS ti
+    ON (ce.emp_no = ti.emp_no)
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY ce.emp_no;
+
+-- Unique Titles
+
+SELECT DISTINCT ON (emp_no) emp_no, first_name, last_name, title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = ('9999-01-01')
+ORDER BY emp_no ASC;
+
+SELECT * FROM unique_titles
+
+-- Count Title
+
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+
+-- Deliverable 2 Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (e.emp_no) e.emp_no,
 e.first_name,
 e.last_name,
